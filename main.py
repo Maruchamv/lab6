5c7439f0500fa634395a5aa45e07d74728a71246
Maruchamv <mmoncadavergara@uf.edu>
2023-07-18 09:47:39 -0400

    Primer intento del lab 6

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 # lab 6
-print('Hey') # test
-# estoy tratando de entender esto wiii
+run_program = True
+While True:
+    print('Menu'
+          '-------------'
+          '1. Encode'
+          '2. Decode'
+          '3. Quit'
+          )
+    option = int(input('Please enter an option: ')
+    if option == 1:
+        pass
+    elif option == 2:
+        pass
+    elif option == 3:
+        pass
