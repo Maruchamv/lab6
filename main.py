eb8f238e12f3ebfc0245c7fc99ec019ddd49b04f
Alexis Morales Amaro <a.moralesamaro@ufl.edu>
2023-07-18 12:45:50 -0400

    program

--- main.py
+++ main.py
@@ -1,20 +1,64 @@
-# lab 6
-run_program = True
-While True:
-    print('Menu'
-          '-------------'
-          '1. Encode'
-          '2. Decode'
-          '3. Quit'
-          )
-    option = int(input('Please enter an option: ')
-    if option == 1:
-        old_ password = int(input('Please enter your password to encode: '))
-        print('Your password has been encoded and stored')
-    elif option == 2:
-        new_password = 1
-        print(f'The encoded password is {new_password}, and the original password is {old_password})
-    elif option == 3:
-        break
+# COP3502C-Lab6
+# Author: Alexis Morales
+# Author: Maria Moncada
+# Date: July 18, 2023
 
-    # make a comment 777
+
+def decode(num):
+    if num == 10:
+        i = 0
+    elif num == 11:
+        i = 1
+    elif num == 12:
+        i = 2
+    elif num == 13:
+        i = 3
+    else:
+        i = num
+
+    return i
+
+
+def new_password(password):
+    new_password = list()
+    for i in password:
+        i = int(i) + 3
+        new_password.append(str(decode(i)))
+
+    new_pass = "".join(new_password)
+
+    return new_pass
+
+
+def main():
+    new_pass = ""
+    old_password = ""
+    while True:
+        print("\nMenu"
+              "\n-------------" 
+              "\n1. Encode" 
+              "\n2. Decode" 
+              "\n3. Quit")
+
+        option = int(input("Please enter an option: "))
+
+        if option == 1:
+            print("Please enter your password to encode:", end=" ")
+            old_password = [x for x in input()]
+            print("Your password has been encoded and stored!")
+
+            new_pass = new_password(old_password)
+            continue
+
+        old_password = "".join(old_password)
+
+        if option == 2:
+            print(
+                f"The encoded password is {new_pass}, and the original password is {old_password}."
+            )
+        if option == 3:
+            return
+
+
+if __name__ == "__main__":
+    main()
